5d037b6fb60384b480f97b159cdf9a1c1970a0cf
edisonqkj <edison90110@gmail.com>
2014-04-12 13:15:02 +0800

    parallel split process

--- splitdem.py
+++ splitdem.py
@@ -5,6 +5,7 @@ import os
 import datetime
 import string
 import sys
+from multiprocessing import Pool
 from test_clearfile import *
 
 import arcpy
@@ -14,21 +15,14 @@ from arcpy.sa import *
 #print 'level: '+sys.args[1]
 
 #level=sys.args[1]
-def Process(in_dem,in_envelope):
-    workspace='F:/'
-
+def region(workspace,in_envelope):
+    ## index envelops into four regions
+    ## based on the craters' (x,y)
     folder_ne=workspace+"north-east/"
     folder_se=workspace+"south-east/"
     folder_nw=workspace+"north-west/"
     folder_sw=workspace+"south-west/"
-
-    in_dem="E:/R/Moon/LOLA/Moon_LRO_LOLA_global_LDEM_118m_Feb2013.cub"
-    in_envelope='E:/tmp/env1.shp'
-
-    starttime_all = datetime.datetime.now()
-
-    env.workspace = workspace
-
+    
     if os.path.exists(folder_ne):
         CleanDir(folder_ne,False)
     os.mkdir(folder_ne)
@@ -42,9 +36,8 @@ def Process(in_dem,in_envelope):
         CleanDir(folder_sw,False)
     os.mkdir(folder_sw)
     print("Directories is ready......")
-############################ Body ###########################
-    print 'Splitting DEM starts......'
-    starttime0 = datetime.datetime.now()
+
+    env.workspace = workspace
     records=arcpy.SearchCursor(in_envelope,"","","","")
 
     id_ne=[]
@@ -55,66 +48,18 @@ def Process(in_dem,in_envelope):
     for record in records:
         #if len(id_nw)>5:
         #    break
-        '''
-        1. iterate to select an envelope from envelopes layer
-        '''
         lon=float(record.Lon_E)
         lat=float(record.Lat)
         if lon>0:
             if lat>0:
-                target_dir=folder_ne+str(record.ORIG_FID)+"/"
-                if not os.path.exists(target_dir):
-                    os.mkdir(target_dir)
-                outshp=target_dir+"env"+str(record.ORIG_FID)+".shp"
                 id_ne.append(record.ORIG_FID)
             elif lat<0:
-                target_dir=folder_se+str(record.ORIG_FID)+"/"
-                if not os.path.exists(target_dir):
-                    os.mkdir(target_dir)
-                outshp=target_dir+"env"+str(record.ORIG_FID)+".shp"
                 id_se.append(record.ORIG_FID)
         elif lon<0:
             if lat>0:
-                target_dir=folder_nw+str(record.ORIG_FID)+"/"
-                if not os.path.exists(target_dir):
-                    os.mkdir(target_dir)
-                outshp=target_dir+"env"+str(record.ORIG_FID)+".shp"
                 id_nw.append(record.ORIG_FID)
             elif lat<0:
-                target_dir=folder_sw+str(record.ORIG_FID)+"/"
-                if not os.path.exists(target_dir):
-                    os.mkdir(target_dir)
-                outshp=target_dir+"env"+str(record.ORIG_FID)+".shp"
                 id_sw.append(record.ORIG_FID)
-
-        arcpy.env.extent=in_envelope
-        where_clause = '"ORIG_FID" = '+str(record.ORIG_FID)
-        arcpy.Select_analysis(in_envelope, outshp, where_clause)
-
-        '''
-        2. extract DEM in the envelope
-        '''
-        out_dem=target_dir+"dem"+str(record.ORIG_FID)
-
-        arcpy.env.extent=outshp
-        arcpy.env.snapRaster=in_dem
-        # Check out the ArcGIS Spatial Analyst extension license
-        arcpy.CheckOutExtension("Spatial")
-
-        # Execute ExtractByMask
-        outExtractByMask = ExtractByMask(in_dem, outshp)
-        # Save the output 
-        outExtractByMask.save(out_dem)
-
-        '''
-        3. convert DEM to Ascii
-        '''
-        out_asc=target_dir+"asc"+str(record.ORIG_FID)+".txt"
-        Raster2Ascii(out_dem,out_asc,False)
-
-        print("Finished Area: "+str(record.ORIG_FID)+"......")
-
-
     ids_path=[folder_ne+'ids.txt',\
               folder_se+'ids.txt',\
               folder_nw+'ids.txt',\
@@ -123,17 +68,83 @@ def Process(in_dem,in_envelope):
     #print(ids)
 
     map(writeids,ids_path,ids)
-    print("write is finished......")
+    print("Indexing is finished......")
 
-    ################################ end ##################################
+    return ids_path
 
-    endtime_all = datetime.datetime.now()
-    print 'All processes are finished......'
-    print 'Cost time: '+str((endtime_all - starttime_all).seconds)+' seconds'
-    
-    f=open("costtime.txt",'a')
-    f.writelines('Split Cost time: '+str((endtime_all - starttime_all).seconds)+' seconds')
+def start(paras_father):
+    print("paras_father")
+    print(paras_father)
+    in_dem=paras_father[0]
+    in_envelope=paras_father[1]
+    ids_txt=paras_father[2]
+
+    filedir,filename=os.path.split(ids_txt)# .../north-east
+    name=filename.split('.')[0]# ids
+    format=filename.split('.')[1]# txt
+
+    #print("Current: "+filedir)
+
+    f=open(ids_txt)
+    paras_son=map(lambda x:[in_dem,in_envelope,filedir+"/"+str(int(x))+"/"],f.readlines()[0][1:-1].split(','))
     f.close()
+    print("Area Numbers: "+str(len(paras_son)))
+    #print(paras_son[0])
+    #print(len(paras_son))
+    #paras_son=paras_son[0:5]
+    process_num=4
+    print("Process Numbers: "+str(process_num))
+    #print("slice_paras:")
+    slice_paras=map(lambda x:paras_son[x:x+process_num],\
+                    range(0,len(paras_son),process_num))
+    #print(slice_paras)
+    map(parallel,slice_paras)
+    print(ids_txt+" is finished......")
+
+def parallel(paras):
+    pool = Pool(len(paras))
+    pool.map(split,paras)
+    pool.close()
+    pool.join()
+
+def split(paras):
+    in_dem=paras[0]
+    in_envelope=paras[1]
+    target_dir=paras[2]
+    ORIG_FID=target_dir.split('/')[-2]
+    #print("target_dir: "+target_dir)
+
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    outshp=target_dir+"env"+ORIG_FID+".shp"
+
+    env.workspace = target_dir
+    arcpy.env.extent=in_envelope
+    where_clause = '"ORIG_FID" = '+ORIG_FID
+    arcpy.Select_analysis(in_envelope, outshp, where_clause)
+
+    '''
+    2. extract DEM in the envelope
+    '''
+    out_dem=target_dir+"dem"+ORIG_FID
+
+    arcpy.env.extent=outshp
+    arcpy.env.snapRaster=in_dem
+    # Check out the ArcGIS Spatial Analyst extension license
+    arcpy.CheckOutExtension("Spatial")
+
+    # Execute ExtractByMask
+    outExtractByMask = ExtractByMask(in_dem, outshp)
+    # Save the output 
+    outExtractByMask.save(out_dem)
+
+    '''
+    3. convert DEM to Ascii
+    '''
+    out_asc=target_dir+"asc"+ORIG_FID+".txt"
+    Raster2Ascii(out_dem,out_asc,False)
+
+    print("Finished Area: "+ORIG_FID+"......")
 
 def writeids(path,data):
     if len(data)>0:
@@ -147,5 +158,26 @@ def Raster2Ascii(raster,ascii,isprint):
         print ('Raster2Ascii is finished....')
 
 if __name__=='__main__':
-    Process("","")
-    os.system('shutdown -s -f -t 10')
+
+    starttime_all = datetime.datetime.now()
+    ############################### body ##################################
+    ## 1. index envelops into four regions
+    save_dir="E:/"
+    envelope='E:/tmp/env1.shp'
+    ids_path=region(save_dir,envelope)
+
+    dem="E:/R/Moon/LOLA/Moon_LRO_LOLA_global_LDEM_118m_Feb2013.cub"
+    paras=map(lambda x:[dem,envelope,x],ids_path)
+    #print(paras)
+    map(start,paras)
+    ################################ end ##################################
+
+    endtime_all = datetime.datetime.now()
+    print 'All processes are finished......'
+    print 'Cost time: '+str((endtime_all - starttime_all).seconds)+' seconds'
+    
+    f=open(save_dir+"costtime.txt",'a')
+    f.writelines('Split Cost time: '+str((endtime_all - starttime_all).seconds)+' seconds')
+    f.close()
+
+    #os.system('shutdown -s -f -t 10')
